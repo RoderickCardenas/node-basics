150fd7c772bd3618a0100097c2b93e011b8a87ed
Roderick Cardenas <roderickcardenas1@gmail.com>
2020-04-09 03:58:54 -0700

    mkdir writeFile appendFile

---
+++ reference/fs_demo.js
@@ -0,0 +1,31 @@
+const fs = require('fs')
+const path = require('path')
+
+//create folder fs.mkdir(path[,options], callback)
+// sync ver fs.mkdirSync(path[, options])
+
+//create folder named test in current directory
+// fs.mkdir(path.join(__dirname, '/test'), {}, err => {
+//   if (err) throw err
+//   console.log('Folder created...')
+// })
+
+//Create and write to file
+fs.writeFile(
+  path.join(__dirname, '/test', 'hello.txt'),
+  'Hello World!',
+  err => {
+    if (err) throw err
+    console.log('File written to...')
+
+    //file append text
+    fs.appendFile(
+      path.join(__dirname, '/test', 'hello.txt'),
+      ' I love node js',
+      err => {
+        if (err) throw err
+        console.log('File written to...')
+      }
+    )
+  }
+)
